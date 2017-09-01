977b42964968fbd96249489fc1ab10ef21b483df
Ben Lorentz <blorentz@luc.edu>
2017-09-01 15:02:49 -0500

    Part 3 up to last bullet point, I made the fizzBuzz method, I am not sure how to make the test suite but as far as I can tell the program is correct

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -12,25 +12,32 @@ public class Main {
 		System.out.print("Enter Max Number: ");
 		max = reading.nextInt();
 
-		for(int i = 1; i <= max ; i++)
-		{
-			if((i % 3 == 0 && i%5 == 0) && i != 0)
-			{
-				System.out.println("Fizz Buzz");
-			}
-			else if((i % 3 == 0) && i != 0)
-			{
-				System.out.println("Fizz");
-			}
-			else if((i % 5 == 0) && i != 0)
-			{
-				System.out.println("Buzz");
-			}
-			else
-			{
-				System.out.println(i);
-			}
+		fizzBuzz(max);
 		}
+
+		private static int[] fizzBuzz(int n)
+        {
+            String[] storage = new String[n];
+
+            for(int i = 1; i <= n ; i++)
+            {
+                if((i % 3 == 0 && i%5 == 0) && i != 0)
+                {
+                    storage[i] = "Fizz Buzz";
+                }
+                else if((i % 3 == 0) && i != 0)
+                {
+                    storage[i] = "Fizz";
+                }
+                else if((i % 5 == 0) && i != 0)
+                {
+                   storage[i] = "Buzz";
+                }
+                else
+                {
+                   storage[i] = Integer.toString(i);
+                }
+        }
 
 	}
 }
