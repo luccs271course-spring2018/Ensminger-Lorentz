4dc8ec50511e63a4103c298bb08c130b910eeba0
blorentz <blorentz@luc.edu>
2017-09-01 09:59:35 -0500

    This should be on Ben's branch

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,26 +1,10 @@
 package hw;
 
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args)
-	{
-		Scanner reading = new Scanner(System.in);
-		int max = 0;
-
-		System.out.print("Enter Max Number: ");
-		max = reading.nextInt();
-
-		for(int i = 0; i =< max ; i++)
-        {
-            if(i % 3 == 0 && i%5 == 0)
-            {
-                System.out.println("Fizz Buzz");
-            }
-            else if()
-        }
-
+    {
+		System.out.println("This is the beginning of Ben's branch");
 
 	}
 }
