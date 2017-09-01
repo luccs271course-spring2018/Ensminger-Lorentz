fb2d9cc6bf705cd20b8019bdf0546e7e12eb6758
blorentz <blorentz@luc.edu>
2017-09-01 10:12:16 -0500

    This should be on Ben's branch

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,9 +1,26 @@
 package hw;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args)
 	{
-		System.out.println("This is the beginning of the lab");
+		Scanner reading = new Scanner(System.in);
+		int max = 0;
+
+		System.out.print("Enter Max Number: ");
+		max = reading.nextInt();
+
+		for(int i = 0; i =< max ; i++)
+        {
+            if(i % 3 == 0 && i%5 == 0)
+            {
+                System.out.println("Fizz Buzz");
+            }
+            else if()
+        }
+
+
 	}
 }
