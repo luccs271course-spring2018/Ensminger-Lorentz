e033c58eebba39f8e69a77c0a7c18183ce54fa64
blorentz <blorentz@luc.edu>
2017-09-01 14:38:52 -0500

    Ben's first pass at the problem

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,10 +1,36 @@
 package hw;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args)
     {
-		System.out.println("This is the beginning of Ben's branch");
+		Scanner reading = new Scanner(System.in);
+		int max = 0;
+
+		System.out.print("Enter Max Number: ");
+		max = reading.nextInt();
+
+		for(int i = 0; i <= max ; i++)
+		{
+			if((i % 3 == 0 && i%5 == 0) && i != 0)
+			{
+				System.out.println("Fizz Buzz");
+			}
+			else if((i % 3 == 0) && i != 0)
+			{
+				System.out.println("Fizz");
+			}
+			else if((i % 5 == 0) && i != 0)
+			{
+				System.out.println("Buzz");
+			}
+			else
+			{
+				System.out.println(i);
+			}
+		}
 
 	}
 }
