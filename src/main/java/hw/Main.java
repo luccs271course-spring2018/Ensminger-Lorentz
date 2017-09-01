3d366495b125f33be62ed6043653882d5d2acd5d
Ben Lorentz <blorentz@luc.edu>
2017-09-01 15:10:54 -0500

    Part 3 up to last bullet point, I made the fizzBuzz method, I am not sure how to make the test suite but as far as I can tell the program is correct Also, fixing some formatting issues that caused the program not to run.

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -4,40 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args)
-    {
-		Scanner reading = new Scanner(System.in);
-		int max = 0;
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        int max = 0;
 
-		System.out.print("Enter Max Number: ");
-		max = reading.nextInt();
+        System.out.print("Enter Max Number: ");
+        max = reading.nextInt();
 
-		fizzBuzz(max);
-		}
+        fizzBuzz(max);
+    }
 
-		private static int[] fizzBuzz(int n)
-        {
-            String[] storage = new String[n];
+    private static String[] fizzBuzz(int n) {
+        String[] storage = new String[n+1];
 
-            for(int i = 1; i <= n ; i++)
-            {
-                if((i % 3 == 0 && i%5 == 0) && i != 0)
-                {
-                    storage[i] = "Fizz Buzz";
-                }
-                else if((i % 3 == 0) && i != 0)
-                {
-                    storage[i] = "Fizz";
-                }
-                else if((i % 5 == 0) && i != 0)
-                {
-                   storage[i] = "Buzz";
-                }
-                else
-                {
-                   storage[i] = Integer.toString(i);
-                }
+        for (int i = 1; i <= n; i++) {
+            if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
+                storage[i] = "Fizz Buzz";
+            } else if ((i % 3 == 0) && i != 0) {
+                storage[i] = "Fizz";
+            } else if ((i % 5 == 0) && i != 0) {
+                storage[i] = "Buzz";
+            }
+            else {
+                storage[i] = Integer.toString(i);
+            }
+
         }
-
-	}
+        return storage;
+    }
 }
+
