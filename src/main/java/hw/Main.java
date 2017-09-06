c761d7e2f8f574f7bbc9f0af1e46a5a800b2780a
blorentz <blorentz@luc.edu>
2017-09-06 09:06:02 -0500

    Formatting the code block

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -15,17 +15,20 @@ public class Main {
     }
 
     private static String[] fizzBuzz(int n) {
-        String[] storage = new String[n+1];
+        String[] storage = new String[n + 1];
 
         for (int i = 1; i <= n; i++) {
             if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
                 storage[i] = "Fizz Buzz";
-            } else if ((i % 3 == 0) && i != 0) {
+            } else if ((i % 3 == 0) && i != 0)
+
+            {
                 storage[i] = "Fizz";
-            } else if ((i % 5 == 0) && i != 0) {
+            } else if ((i % 5 == 0) && i != 0)
+
+            {
                 storage[i] = "Buzz";
-            }
-            else {
+            } else {
                 storage[i] = Integer.toString(i);
             }
 
