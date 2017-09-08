3936da88173664aea984ccaac93af56f824ec3bf
blorentz <blorentz@luc.edu>
2017-09-08 09:57:00 -0500

    first version of the test method

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,10 +1,39 @@
 package hw;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args)
-    {
-		System.out.println("This is the beginning of Ben's branch");
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        int max = 0;
 
-	}
+        System.out.print("Enter Max Number: ");
+        max = reading.nextInt();
+
+        fizzBuzz(max);
+    }
+
+    public static String[] fizzBuzz(int n) {
+        String[] storage = new String[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
+                storage[i] = "Fizz Buzz";
+            } else if ((i % 3 == 0) && i != 0)
+
+            {
+                storage[i] = "Fizz";
+            } else if ((i % 5 == 0) && i != 0)
+
+            {
+                storage[i] = "Buzz";
+            } else {
+                storage[i] = Integer.toString(i);
+            }
+
+        }
+        return storage;
+    }
 }
+
--- src/test/java/hw/TestHelloWorld.java
+++ src/test/java/hw/TestHelloWorld.java
@@ -6,76 +6,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class TestHelloWorld {
 
-    private Main fixture;
-
-    @Before
-    public void setUp() throws Exception {
-        fixture = new Main();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        fixture = null;
-    }
-
-    @Test
-    public void getMessage() {
-        assertNotNull(fixture);
-        ArrayList<String> storageCheck = new ArrayList<>();
-//        String[] storageCheck= new String{"1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizz buzz", "16","17"};
-        storageCheck.addAll(Arrays.asList("1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz", "buzz", "11", "fizz", "13", "14", "fizz buzz", "16", "17"));
-        assertEquals(storageCheck, fixture.fizzBuzz(17));
-//        assertArrayEquals(storageCheck,fixture.fizzBuzz(n:17));
-
-    }
-	@Test
-	public void getTest1() {
-		assertNotNull(fixture);
-		ArrayList<String> storageCheck = new ArrayList<>();
-//		String[] storageCheck= new String{"1", "2", "fizz", "4", "buzz", "fizz", "7"};
-		storageCheck.addAll(Arrays.asList("1", "2", "fizz", "4", "buzz", "fizz", "7"));
-		assertEquals(storageCheck, fixture.fizzBuzz(7));
-//		assertArrayEquals(storageCheck,fixture.fizzBuzz(n:7));
-
+	private Main fixture;
+	
+	@Before
+	public void setUp() throws Exception {
+		fixture = new Main();
 	}
-	@Test
-	public void getTest2() {
-		assertNotNull(fixture);
-		ArrayList<String> storageCheck = new ArrayList<>();
-//		String[] storageCheck= new String{"1"};
-		storageCheck.add("1");
-		assertEquals(storageCheck, fixture.fizzBuzz(1));
-//		assertArrayEquals(storageCheck,fixture.fizzBuzz(n:1));
 
+	@After
+	public void tearDown() throws Exception {
+		fixture = null;
 	}
-	@Test
-	public void getTest3() {
-		assertNotNull(fixture);
-		ArrayList<String> storageCheck = new ArrayList<>();
-		storageCheck.addAll(Arrays.asList("The integer entered must be greater than or equal to 1"));
-		assertEquals(storageCheck, fixture.fizzBuzz(-10));
 
-	}
 	@Test
-	public void getTest4() {
+	public void getMessage() {
 		assertNotNull(fixture);
-		ArrayList<String> storageCheck = new ArrayList<>();
-		storageCheck.addAll(Arrays.asList("The integer entered must be greater than or equal to 1"));
-		assertEquals(storageCheck, fixture.fizzBuzz(-1));
-
-	}
-	@Test
-	public void getTest5() {
-		assertNotNull(fixture);
-		ArrayList<String> storageCheck = new ArrayList<>();
-		storageCheck.addAll(Arrays.asList("The integer entered must be greater than or equal to 1"));
-		assertEquals(storageCheck, fixture.fizzBuzz(0));
-
+		assertEquals("hello world", fixture.fizzBuzz(5));
 	}
 
 
