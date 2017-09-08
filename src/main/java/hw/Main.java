e4b6011e19479fb960772b6ca9e2eea26b11bcf3
tensminger97 <tmillerensminger@luc.edu>
2017-09-08 12:23:22 -0500

    part3 practice

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,73 +1,42 @@
 package hw;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner reading = new Scanner(System.in);
         int max = 0;
-        if (args.length > 0) {
-            try {
-                max = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.err.println("The argument entered must be an integer ");
-                System.exit(1);
-            }
-        }
+
+        System.out.print("Enter Max Number: ");
+        max = reading.nextInt();
+
         fizzBuzz(max);
     }
 
-    public static List<String> fizzBuzz(int n) {
-        List<String> storage = new ArrayList<>();
-        String message = "The integer entered must be greater than or equal to 1";
-        if (n > 0) {
-            for (int i = 1; i < n + 1; i++) {
-                if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
-                    storage.add("fizz buzz");
-                } else if ((i % 3 == 0) && i != 0)
+    public static String[] fizzBuzz(int n) {
+        String[] storage = new String[n];
 
-                {
-                    storage.add("fizz");
-                } else if ((i % 5 == 0) && i != 0) {
-                    storage.add("buzz");
-                } else {
-                    storage.add(Integer.toString(i));
-                }
-                String output = storage.get(i - 1);
-                System.out.println(output);
+        for (int i = 1; i < n + 1; i++) {
+            if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
+                storage[i-1] = "Fizz Buzz";
+            } else if ((i % 3 == 0) && i != 0)
+
+            {
+                storage[i-1] = "fizz";
             }
-            return storage;
-        } else {
-            storage.add(message);
-            return storage;
+            else if ((i % 5 == 0) && i != 0)
+			{
+                storage[i-1] = "buzz";
+            }
+            else
+			{
+                storage[i-1] = Integer.toString(i);
+            }
+			System.out.println(storage[i-1]);
         }
+        System.out.print(storage);
+        return storage;
     }
 }
-//This is for Part 2 and 3 of the project
- /*   public static String[] fizzBuzz(int n) {
-        String[] storage = new String [n]();
-        String[] message = new String {"The integer entered must be greater than or equal to 1"};
-        if (n > 0) {
-            for (int i = 1; i < n + 1; i++) {
-                if ((i % 3 == 0 && i % 5 == 0) && i != 0) {
-                    storage[n-1]=i;
-                } else if ((i % 3 == 0) && i != 0)
-                {
-                    storage[n-1]=i;
-                } else if ((i % 5 == 0) && i != 0) {
-                    storage[n-1]=i;
-                } else {
-                    storage[n-1]=(Integer.toString(i));
-                }
-                String output = storage[n-1];
-                System.out.println(output);
-            }
-            return storage;
-        } else {
-            return message;
-        }
-    }
-}*/
+
