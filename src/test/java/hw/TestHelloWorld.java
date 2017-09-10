bc22d1b7d1ea60a1d573d5d30f8b44e355ea0fa0
tensminger97 <tmillerensminger@luc.edu>
2017-09-10 10:51:07 -0500

    part3 test complete

--- src/test/java/hw/TestHelloWorld.java
+++ src/test/java/hw/TestHelloWorld.java
@@ -19,15 +19,49 @@ public class TestHelloWorld {
 	public void tearDown() throws Exception {
 		fixture = null;
 	}
+	@Test
+	public void getTest1()
+	{
+		assertNotNull(fixture);
+		String check = "Invalid entry, number must be greater than or equal to 1";
+		assertEquals(check, fixture.fizzbuzz(n:-10));
+	}
+	@Test
+	public void getTest2(){
+		assertNotNull(fixture);
+		String check = "Invalid entry, number must be greater than or equal to 1";
+		assertEquals(check,fixture.fizzbuzz(n:-1));
 
+	}
+	@Test
+	public void getTest3() {
+		assertNotNull(fixture);
+		String check = "Invalid entry, number must be greater than or equal to 1";
+		assertEquals(check, fixture.fizzbuzz(n:0));
+	}
+	@Test
+	public void getTest4() {
+		assertNotNull(fixture);
 
+		String [] storagecheck = {"1"};
+		assertArrayEquals(storagecheck , fixture.fizzBuzz(1));
 
+	}
+
+	@Test
+	public void getTest5() {
+		assertNotNull(fixture);
+
+		String [] storagecheck = {"1", "2", "fizz","4", "buzz","fizz","7"};
+		assertArrayEquals(storagecheck , fixture.fizzBuzz(7));
+
+	}
 	@Test
 	public void getMessage() {
 		assertNotNull(fixture);
 
-		String [] storagecheck = {"1", "2", "fizz","4", "buzz"};
-		assertArrayEquals(storagecheck , fixture.fizzBuzz(5));
+		String [] storagecheck = {"1", "2", "fizz","4", "buzz","fizz","7","8","fizz","buzz","11","fizz","13","14","fizz buzz","16","17"};
+		assertArrayEquals(storagecheck , fixture.fizzBuzz(7));
 
 	}
 
